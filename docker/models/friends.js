eaa442e258df7f51fb6a8193f6ca57fe6587e7e8
ainacb <ainacb.93@gmail.com>
2025-03-20 19:21:23 +1100

    module 10 docker

---
+++ docker/models/friends.js
@@ -0,0 +1,10 @@
+const friends = [
+  { id: 1, name: "Phoebe", gender: "female", letter: "p" },
+  { id: 2, name: "Joey", gender: "male", letter: "j" },
+  { id: 3, name: "Chandler", gender: "male", letter: "c" },
+  { id: 4, name: "Monica", gender: "female", letter: "m" },
+  { id: 5, name: "Ross", gender: "male", letter: "r" },
+  { id: 6, name: "Rachael", gender: "female", letter: "r" },
+];
+
+module.exports = friends;
